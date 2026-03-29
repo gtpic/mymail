df54178c2c72ce7fc186b93ca03b0d2ea52cde33
gtpic <1301701321@qq.com>
2026-03-29 16:18:38 +0800

    Update test-api.js

--- mail-worker/src/api/test-api.js
+++ mail-worker/src/api/test-api.js
@@ -1,1 +1,22 @@
+import app from '../hono/hono';
+import emailService from '../service/email-service';
+import result from '../model/result';
 
+app.get('/test/allEmail/latest', async (c) => {
+	const query = c.req.query();
+    
+	// 从 Cloudflare 的环境变量中获取名为 API_KEY 的值
+	const secretKey = c.env.API_KEY || '';
+    
+	// 校验地址栏传来的 key 是否等于环境变量中的暗号
+	if (query.key !== secretKey) {
+		return c.json(result.fail('暗号错误，无权访问', 401));
+	}
+
+	try {
+		const list = await emailService.allEmailLatest(c, query);
+		return c.json(result.ok(list));
+	} catch (error) {
+		return c.json(result.fail(error.message, 500));
+	}
+});
