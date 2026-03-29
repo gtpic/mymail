0972707ccb8ae9642e6db22f4994c72895877a0a
gtpic <1301701321@qq.com>
2026-03-29 16:38:02 +0800

    Update test-api.js

--- mail-worker/src/api/test-api.js
+++ mail-worker/src/api/test-api.js
@@ -2,7 +2,8 @@ import app from '../hono/hono';
 import emailService from '../service/email-service';
 import result from '../model/result';
 
-app.get('/test/allEmail/latest', async (c) => {
+// 【修改点1】接口路径建议改为 list，更符合其强大的查询功能
+app.get('/test/allEmail/list', async (c) => {
 	const query = c.req.query();
     
 	// 从 Cloudflare 的环境变量中获取名为 api_key 的值
@@ -14,8 +15,9 @@ app.get('/test/allEmail/latest', async (c) => {
 	}
 
 	try {
-		const list = await emailService.allEmailLatest(c, query);
-		return c.json(result.ok(list));
+		// 【修改点2】将原来的 allEmailLatest 替换为底层支持无数个搜索条件的 allList
+		const data = await emailService.allList(c, query);
+		return c.json(result.ok(data));
 	} catch (error) {
 		return c.json(result.fail(error.message, 500));
 	}
